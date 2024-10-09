aca603d1eb2affc5e2d1ea6216c93228dcfdac06
erle duval <erle.duval@supdevinci-edu.fr>
2024-10-09 16:32:01 +0200

    Add heroicons

--- tinpets-frontend/src/components/Footer/Footer.jsx
+++ tinpets-frontend/src/components/Footer/Footer.jsx
@@ -3,8 +3,9 @@ const Footer = () => {
         <div className="flex justify-around items-center bg-rouge-projet h-16">
             <p>Mentions légales</p>
             <p>A propos de nous</p>
+            <User />
         </div>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
