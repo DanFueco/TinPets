d1c4224df03fa984487e620d2b5d6c48ecb452b8
Dany PHILIPPE <kerdanphilippe22@gmail.com>
2024-10-09 16:46:40 +0200

    J'ai ajouté heroicons erlé

--- tinpets-frontend/src/Pages/Swipe/Swipe.jsx
+++ tinpets-frontend/src/Pages/Swipe/Swipe.jsx
@@ -1,7 +1,7 @@
 const Swipe = () => {
     return (
       <div className="flex flex-col flex-grow items-center bg-vert-caca">
-        <div className="bg-rose-projet w-80 px-5 my-8 flex-grow">
+        <div className="bg-blanc-projet w-80 px-5 py-5 my-8 flex-grow drop-shadow-xl rounded-md flex-col">
           <p>Nom de l'Animal</p>
           <p>Espèce</p>
         </div>
